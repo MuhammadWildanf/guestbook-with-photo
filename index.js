e1d61a981ddf47417935086ff8d96ab42bee8757
Muhammad Wildan Ferdiansyah <92373802+MuhammadWildanf@users.noreply.github.com>
2025-12-27 20:54:19 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const multer = require("multer");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Firebase Admin
 admin.initializeApp({
